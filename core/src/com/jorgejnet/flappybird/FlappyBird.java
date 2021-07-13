83b9d8e7fdcb5e3a9c8cf3597d45986102540d1b
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2021-07-13 14:50:29 +0200

    Implementacion MenuState.

--- core/src/com/jorgejnet/flappybird/FlappyBird.java
+++ core/src/com/jorgejnet/flappybird/FlappyBird.java
@@ -1,9 +1,11 @@
 package com.jorgejnet.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.jorgejnet.flappybird.States.GameStateManager;
+import com.jorgejnet.flappybird.States.MenuState;
 
 public class FlappyBird extends ApplicationAdapter {
 
@@ -11,26 +13,26 @@ public class FlappyBird extends ApplicationAdapter {
 	public static final int HEIGHT = 720;
 	public static final String TITLE  = "FlappyBird para Android";
 
+	private GameStateManager gsm;
 	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/jorgejnet/flappybird/States/MenuState.java
@@ -0,0 +1,25 @@
+package com.jorgejnet.flappybird.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gameStateManager) {
+        super(gameStateManager);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+}
