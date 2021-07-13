ccbc0ee70ba3cfa6e0089c35561ff33dd0764a3d
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2021-07-13 19:32:54 +0200

    Background Playstate.

--- core/src/com/jorgejnet/flappybird/states/PlayState.java
+++ core/src/com/jorgejnet/flappybird/states/PlayState.java
@@ -1,6 +1,7 @@
 package com.jorgejnet.flappybird.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jorgejnet.flappybird.FlappyBird;
 import com.jorgejnet.flappybird.sprites.Bird;
@@ -8,11 +9,13 @@ import com.jorgejnet.flappybird.sprites.Bird;
 public class PlayState extends State {
 
     private Bird bird;
+    private Texture bg;
 
     public PlayState(GameStateManager gameStateManager) {
         super(gameStateManager);
         bird = new Bird(50, 320);
         camera.setToOrtho(false, FlappyBird.WIDTH / 2, FlappyBird.HEIGHT / 2);
+        bg = new Texture("bg.png");
     }
 
     @Override
@@ -32,6 +35,8 @@ public class PlayState extends State {
     public void render(SpriteBatch spriteBatch) {
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
+        spriteBatch.draw(bg, camera.position.x - (camera.viewportWidth/2), camera.position.y - (camera.viewportHeight/2));
+        //spriteBatch.draw(bg, camera.position.x - (camera.viewportWidth / 2),camera.position.y - (camera.viewportHeight / 2), FlappyBird.WIDTH / 2, FlappyBird.HEIGHT /2);
         spriteBatch.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
         spriteBatch.end();
     }
