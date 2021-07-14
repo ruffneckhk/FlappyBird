9880f8d14ca714c8edce88dc5df6a6aa123126ad
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2021-07-14 17:30:46 +0200

    Reposicionamiento del Ground.

--- core/src/com/jorgejnet/flappybird/states/PlayState.java
+++ core/src/com/jorgejnet/flappybird/states/PlayState.java
@@ -13,6 +13,7 @@ public class PlayState extends State {
 
     private static final int TUBE_SPACING = 125;
     private static final int TUBE_COUNT = 4;
+    private static final int GROUND_Y_OFFSET = -30;
 
     private Bird bird;
     private Texture bg;
@@ -30,8 +31,8 @@ public class PlayState extends State {
         bg = new Texture("bg.png");
         tubes = new Array<Tube>();
         ground = new Texture("ground.png");
-        groundPos1 = new Vector2(camera.position.x - camera.viewportWidth / 2, 0);
-        groundPos2 = new Vector2((camera.position.x - camera.viewportWidth / 2) + ground.getWidth(), 0);
+        groundPos1 = new Vector2(camera.position.x - camera.viewportWidth / 2, GROUND_Y_OFFSET);
+        groundPos2 = new Vector2((camera.position.x - camera.viewportWidth / 2) + ground.getWidth(), GROUND_Y_OFFSET);
 
         for (int i = 1; i <= TUBE_COUNT; i++) {
             tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
@@ -48,6 +49,7 @@ public class PlayState extends State {
     @Override
     public void update(float dt) {
         handleInput();
+        updateGround();
         bird.update(dt);
 
         camera.position.x = bird.getPosition().x + 80;
@@ -60,6 +62,10 @@ public class PlayState extends State {
             if (tube.collides(bird.getBounds())) {
                 gsm.set(new MenuState(gsm));
             }
+        }
+
+        if (bird.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET) {
+            gsm.set(new MenuState(gsm));
         }
 
         camera.update();
@@ -84,10 +90,21 @@ public class PlayState extends State {
     @Override
     public void dispose() {
         bg.dispose();
+        ground.dispose();
         bird.dispose();
         for (Tube tube : tubes) {
             tube.dispose();
         }
         System.out.println("PLAY STATE DISPOSE");
+    }
+
+    private void updateGround() {
+        if (camera.position.x - (camera.viewportWidth / 2) > groundPos1.x + ground.getWidth()) {
+            groundPos1.add(ground.getWidth() * 2, 0);
+        }
+
+        if (camera.position.x - (camera.viewportWidth / 2) > groundPos2.x + ground.getWidth()) {
+            groundPos2.add(ground.getWidth() * 2, 0);
+        }
     }
 }
