6552ef6a434c57e4c6d058b12641fe360342018f
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2021-07-14 18:21:59 +0200

    Implementada musica.

--- core/src/com/jorgejnet/flappybird/FlappyBird.java
+++ core/src/com/jorgejnet/flappybird/FlappyBird.java
@@ -2,6 +2,7 @@ package com.jorgejnet.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jorgejnet.flappybird.states.GameStateManager;
@@ -11,17 +12,19 @@ public class FlappyBird extends ApplicationAdapter {
 
 	public static final int WIDTH = 480;
 	public static final int HEIGHT = 720;
-	public static final String TITLE  = "FlappyBird para Android";
+	public static final String TITLE = "FlappyBird para Android";
+	private Music music;
 
 	private GameStateManager gsm;
 	SpriteBatch batch;
 
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
 		Gdx.gl.glClearColor(0, 1, 0, 1);
 		gsm.push(new MenuState(gsm));
+		setUpMusic();
 	}
 
 	@Override
@@ -30,9 +33,17 @@ public class FlappyBird extends ApplicationAdapter {
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
+		music.dispose();
+	}
+
+	private void setUpMusic() {
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.2f);
+		music.play();
 	}
 }
