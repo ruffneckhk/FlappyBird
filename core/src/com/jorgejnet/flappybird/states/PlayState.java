bcc006251ea88f84484887bb574eb8ff31aa54c1
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2021-07-14 15:06:01 +0200

    Agregados tubos al Playstate.

--- core/src/com/jorgejnet/flappybird/states/PlayState.java
+++ core/src/com/jorgejnet/flappybird/states/PlayState.java
@@ -5,17 +5,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jorgejnet.flappybird.FlappyBird;
 import com.jorgejnet.flappybird.sprites.Bird;
+import com.jorgejnet.flappybird.sprites.Tube;
 
 public class PlayState extends State {
 
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gameStateManager) {
         super(gameStateManager);
         bird = new Bird(50, 320);
         camera.setToOrtho(false, FlappyBird.WIDTH / 2, FlappyBird.HEIGHT / 2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -38,6 +41,8 @@ public class PlayState extends State {
         spriteBatch.draw(bg, camera.position.x - (camera.viewportWidth/2), camera.position.y - (camera.viewportHeight/2));
         //spriteBatch.draw(bg, camera.position.x - (camera.viewportWidth / 2),camera.position.y - (camera.viewportHeight / 2), FlappyBird.WIDTH / 2, FlappyBird.HEIGHT /2);
         spriteBatch.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+        spriteBatch.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        spriteBatch.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         spriteBatch.end();
     }
 
