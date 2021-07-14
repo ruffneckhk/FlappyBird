e99dc364c6fb5f7056a72655d7a29e659847ccec
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2021-07-14 15:40:53 +0200

    Agregado movimiento al bird;

--- core/src/com/jorgejnet/flappybird/states/PlayState.java
+++ core/src/com/jorgejnet/flappybird/states/PlayState.java
@@ -42,11 +42,15 @@ public class PlayState extends State {
         handleInput();
         bird.update(dt);
 
+        camera.position.x = bird.getPosition().x + 80;
+
         for (Tube tube : tubes) {
             if (camera.position.x - (camera.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()) {
                 tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
             }
         }
+
+        camera.update();
     }
 
     @Override
