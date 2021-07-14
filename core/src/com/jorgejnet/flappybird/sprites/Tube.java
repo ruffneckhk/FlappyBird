0a0c5db4a3ba017b7cc45c0e23f5f0caf2ec244a
Jorge Juan Antolino Guil <jorgejnet@gmail.com>
2021-07-14 16:19:43 +0200

    Agregados marcos de colision a los tubos.

--- core/src/com/jorgejnet/flappybird/sprites/Tube.java
+++ core/src/com/jorgejnet/flappybird/sprites/Tube.java
@@ -1,6 +1,7 @@
 package com.jorgejnet.flappybird.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.Random;
@@ -18,6 +19,8 @@ public class Tube {
     private Vector2 posBotTube;
 
     private Random rand;
+    private Rectangle boundsTop;
+    private Rectangle boundsBot;
 
     public Tube(float x) {
         topTube = new Texture("toptube.png");
@@ -27,11 +30,21 @@ public class Tube {
 
         posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
         posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+        boundsBot = new Rectangle(posBotTube.x, posBotTube.y, bottomTube.getWidth(), bottomTube.getHeight());
     }
 
     public void reposition(float x) {
         posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
         posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
+        boundsTop.setPosition(posTopTube.x, posTopTube.y);
+        boundsBot.setPosition(posBotTube.x, posBotTube.y);
+    }
+
+    public boolean collides(Rectangle player) {
+        return player.overlaps(boundsTop) || player.overlaps(boundsBot);
     }
 
     public Texture getTopTube() {
